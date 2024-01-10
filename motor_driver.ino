57853143172dad2c03f032520acf80a37c886e5d
Lauren Gilfillan <lag179@pitt.edu>
2024-01-10 12:16:03 -0500

    finsihed, waiting to test with motor

--- motor_driver.ino
+++ motor_driver.ino
@@ -5,7 +5,7 @@
  */
 
 // defines pins
-#define stepPin 2
+#define stepPin 2 
 #define dirPin 5 
  
 void setup() {
@@ -20,47 +20,47 @@ void setup() {
 
 
 void loop() {
-  // want to add later, have the serial terminal tell what position we are currently as
+  // Serial Terminal to tell what position to go to
   if (Serial.available() > 0) {
+    //serial tell us what 
     nextpos = Serial.read() - '0'
     Serial.print("Next Spot to Go to: ");
-    Serial.println(nextpos);
-  }
-  //number of rotations
-  rotvalue = current position - next position
-
-  //for efficencey we make sure we are taking the fastest possible task
-  if (rotvalue == -4) {
-    rotvalue = 1;
-  }
-  else if(rotvalue = 4)
-  {
-    rotvalue = -1;
-  }
-
-  if(rotvalue > 0){
-    digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
-  }
-  else if (rotvalue < 0{
-    digitalWrite(dirPin,LOW)
-  }
-
-
+    Serial.println(nextpos,DEC);
   
-  // Makes 200 pulses for making one full cycle rotation of the gear
-  //we have a 5 to one gear ratio, so to in order to get 1 full rotation of the rack, we need to do 1000 pulses to do one full rotation
-  // to do 1/5 rotation 10000/ 5 --> 200 pulses of 1/5 roataion
+    //number of rotations
+    rotvalue = current position - next position
 
-  int pulse = 200 * abs(rotvalue) * 4
+    //for efficencey we make sure we are taking the fastest possible task
+    if (rotvalue == -4) {
+      rotvalue = 1;
+    }
+    else if(rotvalue = 4)
+    {
+      rotvalue = -1;
+    }
 
-  //so based on rot value we determine the pulse value
+    if(rotvalue > 0){
+      digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+    }
+    else if (rotvalue < 0{
+      digitalWrite(dirPin,LOW)
+    }
+    
+    // Makes 200 pulses for making one full cycle rotation of the gear
+    //we have a 5 to one gear ratio, so to in order to get 1 full rotation of the rack, we need to do 1000 pulses to do one full rotation
+    // to do 1/5 rotation 10000/ 5 --> 200 pulses of 1/5 roataion
 
-  for(int x = 0; x < pulse ; x++) { //multiply pulse value by 4
-    digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(700);    // by changing this time delay between the steps we can change the rotation speed
-    digitalWrite(stepPin,LOW); 
-    delayMicroseconds(700); 
+    int pulse = 200 * abs(rotvalue) * 4
+
+    //so based on rot value we determine the pulse value
+
+    for(int x = 0; x < pulse ; x++) { //multiply pulse value by 4
+      digitalWrite(stepPin,HIGH); 
+      delayMicroseconds(700);    // by changing this time delay between the steps we can change the rotation speed
+      digitalWrite(stepPin,LOW); 
+      delayMicroseconds(700); 
+    }
+    delay(1000); // One second delay
   }
-  delay(1000); // One second delay
 
 }
